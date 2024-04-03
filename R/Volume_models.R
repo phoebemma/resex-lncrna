e40c4ff9ed0079d9398ec3959035f46cbc312df1
Chidimma Echebiri <chidimma.atukpawurgmail.com>
2024-04-03 03:56:23 +0200

    updated to plot images as grid

--- R/Volume_models.R
+++ R/Volume_models.R
@@ -16,7 +16,7 @@ mod_eval <-  model_eval(Vol_all)
  
  #get the model summaries
  
- model_sum <- model_sum(Vol_all, 10)
+ mod_sum <- model_sum(Vol_all, 10)
  
  #colnames(model_sum)
  
@@ -46,26 +46,112 @@ mod_eval <-  model_eval(Vol_all)
  
  
  #merge the model summaries and model evaluation into one dataframe
-Vol_all <- filt_model_parameters(mod_eval, model_sum) %>%
+Vol_all <- filt_model_parameters(mod_eval, mod_sum) %>%
   dplyr::filter(coef != "efflibsize")
  
  unique(Vol_all$coef)
  
  
  #save the filtered coefs 
- #saveRDS(Vol_all, file = "./data/models/Filtered_coefs/Vol_model_all_coefs.RDS")
+# saveRDS(Vol_all, file = "./data/models/Filtered_coefs/Vol_model_all_coefs.RDS")
  
  
  
- #Select those differentially expressed at time t4
- t4 <- Vol_all %>%
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ #Load the model that was normalised based on only lncRNAs
+ Vol_lncs <- readRDS("data/models/vol_model_lncRNAs.RDS")
+ 
+ 
+ #get the model evaluation
+ mod_eval_lncs <-  model_eval(Vol_lncs)
+ 
+ 
+ 
+ #get the model summaries
+ 
+ mod_sum_lncs<- model_sum(Vol_lncs, 10)
+ 
+ 
+ 
+ Vol_lncs <- filt_model_parameters(mod_eval_lncs, mod_sum_lncs) %>%
+   dplyr::filter(coef != "efflibsize_lncs")
+ 
+ unique(Vol_lncs$coef)
+ 
+ #save the filtered coefs 
+ # saveRDS(Vol_lncs, file = "./data/models/Filtered_coefs/Vol_model_lncs.RDS")
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ #Select those differentially expressed at time t4 for vol_all
+ t4_all <- Vol_all %>%
    dplyr::filter(coef == "timet4")
+ 
+ 
+ t4_all_plot <- plot_volcano(t4_all, "DE lncs at time t4 when all genes were used in normalisation")
+ 
+ #select t4 for lncs 
+ t4_lncs <- Vol_lncs %>%
+   dplyr::filter(coef == "timet4")
+ 
+ t4_lncs_plot <- plot_volcano(t4_lncs, "DE at time t4 when lncs used in normalisation")
+ 
+ grid.arrange(t4_all_plot, t4_lncs_plot, ncol = 2)
  
  #make a volcano plot using the plot_volcano function
  jpeg(filename = "./plots/DE_all_model_at_t4.jpeg",
       width = 850, height = 500, quality = 100)
  plot_volcano(t4, "DE lncs at time t4 when all genes were used in normalisation")
  dev.off()
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  
  
  
