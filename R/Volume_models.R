f69247fde471fba56c98c982a34a2c38bc0750fb
Chidimma Echebiri <chidimma.atukpawurgmail.com>
2024-04-03 03:29:57 +0200

    updated and renamed script to run both volume models

---
+++ R/Volume_models.R
@@ -0,0 +1,109 @@
+#| #Load the needed libraries
+source("R/libraries.R")
+
+#Load the functions most regularly used
+source("R/Trainome_functions.R")
+
+
+#Load the raw model
+Vol_all <- readRDS("data/models/vol_model_all.RDS") 
+
+
+#get the model evaluation
+mod_eval <-  model_eval(Vol_all)
+ 
+ 
+ 
+ #get the model summaries
+ 
+ model_sum <- model_sum(Vol_all, 10)
+ 
+ #colnames(model_sum)
+ 
+#  mod_eval %>%
+#    ggplot(aes(pval.unif)) + geom_histogram() +
+#    ggtitle("contratrain gene level model evaluation (normalised at all genes level)")+
+#    theme(axis.text = element_text(size = 15), text = element_text(size = 15),
+#          plot.title = element_text(hjust = 0.5))
+#  
+# mod_eval%>%
+#    ggplot(aes(pval.disp)) + geom_histogram() +
+#    ggtitle("contratrain gene level model evaluation (normalised at all genes level)")+
+#    theme(axis.text = element_text(size = 15), text = element_text(size = 15),
+#          plot.title = element_text(hjust = 0.5))
+# # 
+# # 
+#  mod_eval %>%
+#    ggplot(aes(pval.zinfl)) + geom_histogram() +
+#    ggtitle("contratrain gene level model evaluation (normalised at all genes level)")+
+#    theme(axis.text = element_text(size = 15), text = element_text(size = 15),
+#          plot.title = element_text(hjust = 0.5))
+#  
+ 
+ 
+ 
+ 
+ 
+ 
+ #merge the model summaries and model evaluation into one dataframe
+Vol_all <- filt_model_parameters(mod_eval, model_sum) %>%
+  dplyr::filter(coef != "efflibsize")
+ 
+ unique(Vol_all$coef)
+ 
+ 
+ #save the filtered coefs 
+ #saveRDS(Vol_all, file = "./data/models/Filtered_coefs/Vol_model_all_coefs.RDS")
+ 
+ 
+ 
+ #Select those differentially expressed at time t4
+ t4 <- Vol_all %>%
+   dplyr::filter(coef == "timet4")
+ 
+ #make a volcano plot using the plot_volcano function
+ jpeg(filename = "./plots/DE_all_model_at_t4.jpeg",
+      width = 850, height = 500, quality = 100)
+ plot_volcano(t4, "DE lncs at time t4 when all genes were used in normalisation")
+ dev.off()
+ 
+ 
+ 
+ 
+ 
+ #Select those differentially expressed at time t3
+ t3 <- Vol_all %>%
+   dplyr::filter(coef == "timet3")
+ 
+ #make a volcano plot using the plot_volcano function
+ jpeg(filename = "./plots/DE_all_model_at_t3.jpeg",
+      width = 850, height = 500, quality = 100)
+ plot_volcano(t3, "DE lncs at time t3 when all genes were used in normalisation")
+ dev.off()
+ 
+ 
+ 
+ 
+ 
+ #Select those differentially expressed at set6
+ set6 <- Vol_all %>%
+   dplyr::filter(coef == "conditionset6")
+ 
+ #make a volcano plot using the plot_volcano function
+ jpeg(filename = "./plots/DE_all_model_at_set6.jpeg",
+      width = 850, height = 500, quality = 100)
+ plot_volcano(set6, "DE lncs of set 6 when all genes were used in normalisation")
+ dev.off()
+ 
+ 
+ #Select those differentially expressed at set3
+ set3 <- Vol_all %>%
+   dplyr::filter(coef == "conditionset3")
+ 
+ #make a volcano plot using the plot_volcano function
+ jpeg(filename = "./plots/DE_all_model_at_set3.jpeg",
+      width = 850, height = 500, quality = 100)
+ plot_volcano(set3, "DE lncs of set 3 when all genes were used in normalisation")
+ dev.off()
+ 
+ 
