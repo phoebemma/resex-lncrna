ce86fdfd6a1a5d0587b209bd9b5defb77e48773e
Chidimma Phoebe Echebiri <chidimma.echebiri@inn.no>
2024-06-05 04:49:43 +0200

    update

--- R/training_models.R
+++ R/training_models.R
@@ -1,7 +1,132 @@
 
 
+
+#This script builds the model based on training status of participants' legs
+
+#It also filters the model parameters
+
 #Load the functions most regularly used
 source("R/Trainome_functions.R")
+
+
+
+#load lncRNAs counts
+
+lncRNAS <- readRDS("data/lncRNA_genes.RDS")
+
+#Load metadata file
+
+ct_metadata <- readRDS("data/contratrain_metadata.RDS")
+
+
+#Description of the variables in the metadata dataframe
+
+
+## lib.size = library size lncRNAs alone
+
+## lib.size_all = library size all biotypes
+
+## norm.factors = normalization factors lncRNAs alone
+
+## norm.factors_all = normalization factors all biotypes
+
+## efflibsize_lncs = effective library size of lncRNAs alone
+
+## efflibsize = effective library size of all biotypes
+
+
+
+
+#argument for a second model that looks at trained versus untrained
+
+args<- list(formula = y ~  efflibsize + training_status*time +(1|participant),
+              family = glmmTMB::nbinom2())
+
+
+
+
+
+
+
+
+#Model for the trained versus untrained 
+
+training_model<- seqwrap(fitting_fun = glmmTMB::glmmTMB,
+                             arguments = args,
+                             data = lncRNAS,
+                             metadata = ct_metadata,
+                             samplename = "seq_sample_id",
+                             summary_fun = sum_fun,
+                             eval_fun = eval_mod,
+                             exported = list(),
+                         save_models = FALSE,
+                         return_models = FALSE,
+                             cores = ncores)
+
+
+
+
+#sneekpeek
+training_model$summaries$`A1BG-AS1`
+
+training_model$errors
+
+training_model$evaluations$`A1BG-AS1`
+
+
+
+
+bind_rows(training_model$summaries) %>%
+  
+  filter(coef == "training_statustrained:timet4") %>%
+  # dim()
+  
+  
+  #mutate(gene = names(cor_model$summaries)) %>%
+  ggplot(aes(Pr...z..)) + geom_histogram()
+
+
+library(marginaleffects)
+
+#predictions() function calculates the regression-adjusted predicted values for every observation
+pre <- predictions(training_model$models$`A1BG-AS1`, type = "link")
+
+
+
+
+#save model in data folder
+#saveRDS(training_model, "data/models/seqwrap_generated_models/training_model.RDS")
+
+#get model evaluation using the in-house for combinaing all model evaluations into a table
+mod_eval <- model_eval(training_model)
+
+
+
+#get the model summary using the created function
+#it takes as input the model name and number of unique coefficients
+#it also filters out the model coefficient called "intercept"
+#creates adjusted p values, log2 fold change and fcld change significant threshold
+mod_sum <-  model_sum(training_model, 7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #Load the raw model
